baec771cdf1bf09cff7c25c26435c2365671f5c3
CollyneJumah <collynejumah2010@gmail.com>
2021-10-22 07:06:21 +0300

    added light background on the NavBar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-white nav sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light nav sticky-top border-bottom shadow-sm">
             <div class="container-fluid">
                 <Link className="navbar-brand" to="/">
                     {/* <img src="https://scontent.fmba5-1.fna.fbcdn.net/v/t1.6435-9/173823938_300281701603090_7294814734384138303_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeE15uS0wPC8DZQtHrxm7eLWKFOXuFAaSfIoU5e4UBpJ8vaWjS5ChaYMzC7_y8Pk2SmGsMzpqTcZXaFRnZuueZ0V&_nc_ohc=rzcFWqpzxYEAX-k6l1m&_nc_ht=scontent.fmba5-1.fna&oh=c70b5c8312262d3317182002185200f3&oe=60B759CD"
